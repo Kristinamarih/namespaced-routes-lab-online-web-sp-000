d3fe69b9267d4841372976e6180cd202760db835
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-02 20:05:40 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,7 @@ class SongsController < ApplicationController
   end
 
   def new
-    if Preference.allow_create_songs == true
+    if Preference.can_create_song?
       @song = Song.new
     else
       redirect_to songs_path
