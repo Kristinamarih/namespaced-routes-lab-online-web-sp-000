efc53d064589768d036f6f2c2ca1361b5647f4f2
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-02 20:13:21 +0000

    Automatically backed up by Learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,10 +1,14 @@
 class Preference < ApplicationRecord
 
+  def self.get_or_create
+    self.first ? self.first : self.create
+  end
+  
   def self.can_create_song?
-    self.allow_create_songs
+    self.get_or_create.allow_create_songs
   end
 
   def self.can_create_artist?
-    self.allow_create_artists
+    self.get_or_create.allow_create_artists
   end
 end
